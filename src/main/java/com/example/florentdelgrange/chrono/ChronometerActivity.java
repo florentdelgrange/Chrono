894e3280034ef881bbb0df5de8f47ff352555488
Florent Delgrange <florent.delgrange@gmail.com>
2015-09-04 16:03:46 +0200

    Adding the Chronometer as an Activity

---
+++ src/main/java/com/example/florentdelgrange/chrono/ChronometerActivity.java
@@ -0,0 +1,48 @@
+package com.example.florentdelgrange.chrono;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by Flo on 04-09-15.
+ */
+public class ChronometerActivity extends Activity {
+
+    private ChronometerTimer timer;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chrono);
+
+        timer = new ChronometerTimer((TextView) findViewById(R.id.textView));
+        final ImageButton playButton = (ImageButton) findViewById(R.id.imageButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (timer.getState()) {
+                    case RUN:
+                        timer.pause();
+                        playButton.setImageResource(R.drawable.playbutton);
+                        break;
+                    default:
+                        timer.run();
+                        playButton.setImageResource(R.drawable.pausebutton);
+                        break;
+                }
+            }
+        });
+        final ImageButton stopButton = (ImageButton) findViewById(R.id.imageButton2);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.stop();
+                playButton.setImageResource(R.drawable.playbutton);
+            }
+        });
+    }
+
+}
--- src/main/java/com/example/florentdelgrange/chrono/MainActivity.java
+++ src/main/java/com/example/florentdelgrange/chrono/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.florentdelgrange.chrono;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ChronometerTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,34 +21,12 @@ public class MainActivity extends AppCompatActivity {
         chronoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.chrono);
-                timer = new ChronometerTimer((TextView) findViewById(R.id.textView));
-                final ImageButton playButton = (ImageButton) findViewById(R.id.imageButton);
-                playButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        switch (timer.getState()){
-                            case RUN:
-                                timer.pause();
-                                playButton.setImageResource(R.drawable.playbutton);
-                                break;
-                            default:
-                                timer.run();
-                                playButton.setImageResource(R.drawable.pausebutton);
-                                break;
-                        }
-                    }
-                });
-                final ImageButton stopButton = (ImageButton) findViewById(R.id.imageButton2);
-                stopButton.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        timer.stop();
-                        playButton.setImageResource(R.drawable.playbutton);
-                    }
-                });
+                //setContentView(R.layout.chrono);
+                Intent intent = new Intent(MainActivity.this, ChronometerActivity.class);
+                startActivity(intent);
             }
         });
+
     }
 
 }
